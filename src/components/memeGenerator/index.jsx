07edcecbe0160f7d526d097156eafd4f3638a3bf
bigimann <eneojogodswill@gmail.com>
2025-09-19 23:13:56 +0100

    new file:   public/troll-face.png 	modified:   src/App.js 	new file:   src/components/memeGenerator/header.jsx 	new file:   src/components/memeGenerator/index.css 	new file:   src/components/memeGenerator/index.jsx 	new file:   src/components/memeGenerator/main.jsx

---
+++ src/components/memeGenerator/index.jsx
@@ -0,0 +1,12 @@
+import Main from "./main";
+import Header from "./header";
+import "./index.css";
+
+export default function Index() {
+  return (
+    <div className="meme-generator">
+      <Header />
+      <Main />
+    </div>
+  );
+}
---
+++ src/components/memeGenerator/main.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function Main() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  function memeDisplay() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const newMemeUrl = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({ ...prevMeme, imageUrl: newMemeUrl }));
+  }
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  return (
+    <main className="main">
+      <div className="form">
+        <label htmlFor="topText">
+          Top Text
+          <input
+            type="text"
+            name="topText"
+            placeholder="One does not simply"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+        </label>
+
+        <label htmlFor="bottomText">
+          Bottom Text
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Walk into Mordor"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
+        </label>
+
+        <button onClick={memeDisplay}>Get a new meme image 🖼</button>
+      </div>
+
+      <div className="meme">
+        <img src={meme.imageUrl} alt="Meme" />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
+}
